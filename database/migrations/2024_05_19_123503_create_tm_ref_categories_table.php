27fda0a1a0a77ca2954f4b6bee5975fbbafa0f66
Muhammad Ilham <ilhamgallagher633@gmail.com>
2024-05-19 19:36:22 +0700

    fet: migrate category

--- database/migrations/2024_05_19_123503_create_tm_ref_categories_table.php
+++ database/migrations/2024_05_19_123503_create_tm_ref_categories_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tm_data_articles', function (Blueprint $table) {
+        Schema::create('tm_ref_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tm_data_articles');
+        Schema::dropIfExists('tm_ref_categories');
     }
 };
